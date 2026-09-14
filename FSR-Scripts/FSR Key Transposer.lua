bef26043bac7891c261490db33df13282c1f2eec
forsemusic1996-cyber <forsemusic1996@gmail.com>
2026-09-14 16:10:29 +0300

    Add feedback link to FSR Key Transposer.lua
    Added a new link for script feedback.

--- FSR-Scripts/FSR Key Transposer.lua
+++ FSR-Scripts/FSR Key Transposer.lua
@@ -10,6 +10,7 @@
                
 * Bug Reports: If you find any errors, please report one of the link below                  
 * Website:     http://forum.cockos.com/showthread.php?t=309129
+               http://reaper-script-feedback.forsemusic1996.workers.dev/
     
 ----------------------------------------------------------------
 
